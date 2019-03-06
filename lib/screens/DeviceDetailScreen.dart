c9a5f9126a73cf5c9ae2756a5e8358543b1346ba
Adolfo Esparza <adolfo9585@hotmail.com>
2019-03-06 01:01:35 -0600

    states

--- lib/screens/DeviceDetailScreen.dart
+++ lib/screens/DeviceDetailScreen.dart
@@ -4,24 +4,59 @@ import 'package:flutter_app_smart_home/screens/DeviceListScreen.dart';
 
 import 'package:http/http.dart' as http;
 
-class DeviceDetailScreen extends StatelessWidget{
+class DeviceDetailScreen extends StatefulWidget{
 
   final Device objDevice;
 
-  var url = "http://127.0.0.1:3000/arduino/interruptor";
-  //var url = "http://10.0.2.2:3000/arduino/interruptor";
 
+  final url = "http://127.0.0.1:3000/arduino/interruptor";
+  //var url = "http://10.0.2.2:3000/arduino/interruptor";
 
   DeviceDetailScreen ({Key key, @required this.objDevice}) : super (key:key);
 
   @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+
+}
+
+
+class _MyHomePageState extends State<DeviceDetailScreen> {
+
+  var _ulrImage = '';
+  var status = 0;
+
+  void _lightSwitch() {
+    setState(() {
+      status == 1 ?
+      _ulrImage ='res/images/lightbulbON.png' :
+      _ulrImage ='res/images/lightbulbOFF.png';
+    });
+
+  }
+
+  void statusOff(){
+    status = 0;
+    http.post(widget.url, body:{'status' : '0','pin' : '${widget.objDevice.pin}','time' : '0'});
+    _lightSwitch();
+  }
+
+  void statusOn(){
+    status = 1;
+    http.post(widget.url, body:{'status' : '1','pin' : '${widget.objDevice.pin}','time' : '0'});
+    _lightSwitch();
+  }
+
+  @override
   Widget build(BuildContext context) {
+
 
     return Scaffold(
         appBar: AppBar(
-          title: Text("${objDevice.name}"),
+          title: Text("${widget.objDevice.name}"),
         ),
-        body: Padding(
+        body:
+        Padding(
             padding: EdgeInsets.all(16.0),
             child: Container(
                 child: Column(
@@ -32,10 +67,17 @@ class DeviceDetailScreen extends StatelessWidget{
                         mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
                           new Image.asset(
-                            objDevice.status == 1 ?
-                            'res/images/lightbulbON.png' :
-                            'res/images/lightbulbOFF.png',
-//                  height: 192.0,
+/*
+                            widget.objDevice.status == 1 ?
+                            _ulrImage ='res/images/lightbulbON.png' :
+                            _ulrImage ='res/images/lightbulbOFF.png',
+                            */
+                            _ulrImage == '' ?
+                            widget.objDevice.status == 1 ?
+                            _ulrImage ='res/images/lightbulbON.png' :
+                            _ulrImage ='res/images/lightbulbOFF.png':
+                            '$_ulrImage',
+//
                             fit: BoxFit.fill,
                           ),
                           const ListTile(
@@ -64,15 +106,14 @@ class DeviceDetailScreen extends StatelessWidget{
 
                     RaisedButton(
                       child: Text('On'),
-                      onPressed: () {
-                        http.post(url, body:{'status' : '1','pin' : '${objDevice.pin}','time' : '0'});
-                        },
+                      onPressed: statusOn,
+
                     ),
+
                     RaisedButton(
                       child: Text('Off'),
-                      onPressed: () {
-                        http.post(url, body:{'status' : '0','pin' : '${objDevice.pin}','time' : '0'});
-                      },
+                      onPressed: statusOff,
+
                     ),
 
                   ],
@@ -81,4 +122,5 @@ class DeviceDetailScreen extends StatelessWidget{
         )
     );
   }
+
 }
